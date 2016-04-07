cfb5aa3446e09fa3a947afb00542c839379eedd8
jacobdr <jacobdr@gmail.com>
2016-04-07 12:02:28 -0400

    First storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,6 @@
 import { configure } from '@kadira/storybook';
+const req = require.context('../tools', true, /story\.js$/)
 
-// function loadStories() {
-//   require('../tools/stories/button');
-//   // require as many stories as you need.
-// }
-const req = require.context('../tools', true, /\.story\.\.js$/)
 function loadStories() {
   req.keys().forEach(req)
 }
