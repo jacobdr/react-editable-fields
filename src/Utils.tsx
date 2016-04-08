877cdbdb792d0d620d8945060f709dba4b9238bd
jacobdr <jacobdr@gmail.com>
2016-04-08 11:46:14 -0400

    Updated component distribution process by adding typescript as a dependency. Also bumped react

--- src/Utils.tsx
+++ src/Utils.tsx
@@ -1,5 +1,13 @@
+import * as React from 'react'
+
 export const getProperty = function(path:string, obj:{[key:string]:any}, safe=true){
     return path.split('.').reduce(function(prev, curr) {
         return !safe ? prev[curr] : (prev ? prev[curr] : undefined);
     }, obj || self);
-}
+};
+
+export const Centered = ({ children }) => (
+    <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        {children}
+    </div>
+);
